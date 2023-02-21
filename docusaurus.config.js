db6eef5fc22de13d047975ac5750f14f0a2f243b
Calvin Wilkinson <kinsondigital@gmail.com>
2023-02-21 13:25:23 +0000

    create version dropdown

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          includeCurrentVersion: false,
         },
         blog: {
           showReadingTime: true,
@@ -84,6 +84,11 @@ const config = {
             label: 'GitHub',
             position: 'right',
           },
+          {
+            type: 'docsVersionDropdown',
+            position: 'right',
+            dropdownActiveClassDisabled: true,
+          },          
         ],
       },
       footer: {
