ca66505e8762409d37e7efc78b0140baacaee68d
Calvin Wilkinson <kinsondigital@gmail.com>
2023-02-21 17:12:12 +0000

    config changes and new icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My Velaptor Site',
+  tagline: 'Velaptor is cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -50,8 +50,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -71,7 +70,7 @@ const config = {
         respectPrefersColorScheme: false,
       },
       navbar: {
-        title: 'My Site',
+        title: 'Velaptor Tutorial',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
